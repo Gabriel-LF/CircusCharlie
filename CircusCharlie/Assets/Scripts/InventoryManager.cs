21c143b5cc69da6cd6fe0ddb7d95a5312b76253d
Gabriel Lo Monaco Honório <75794887+Gabriel-LF@users.noreply.github.com>
2021-07-14 17:12:08 -0300

    AllCharlieSkins

--- CircusCharlie/Assets/Scripts/InventoryManager.cs
+++ CircusCharlie/Assets/Scripts/InventoryManager.cs
@@ -11,10 +11,12 @@ public class InventoryManager : MonoBehaviour
     private GameObject preview;
     public ItemType type;
     public GameObject myButton;
+    public Text myName;
 
     void Start()
     {
         preview = transform.parent.gameObject.transform.GetChild(1).gameObject;
+        myName = transform.parent.gameObject.transform.GetChild(2).gameObject.GetComponent<Text>();
 
         foreach (Skin skin in itemList)
         {
@@ -22,25 +24,26 @@ public class InventoryManager : MonoBehaviour
             button.transform.SetParent(transform);
             button.gameObject.GetComponent<Image>().sprite = skin.icon;
             if (type == ItemType.Char)
-                button.gameObject.GetComponent<Button>().onClick.AddListener(delegate { MainMenu.Instance.charEquiped = skin.id; UpdateSkin(); });
+                button.gameObject.GetComponent<Button>().onClick.AddListener(delegate { MainMenu.Instance.charEquiped = skin.id; UpdateSkin(skin.name); });
             if (type == ItemType.Lion)
-                button.gameObject.GetComponent<Button>().onClick.AddListener(delegate { MainMenu.Instance.lionEquiped = skin.id; UpdateSkin(); });
+                button.gameObject.GetComponent<Button>().onClick.AddListener(delegate { MainMenu.Instance.lionEquiped = skin.id; UpdateSkin(skin.name); });
             if (type == ItemType.Horse)
-                button.gameObject.GetComponent<Button>().onClick.AddListener(delegate { MainMenu.Instance.horseEquiped = skin.id; UpdateSkin(); });
+                button.gameObject.GetComponent<Button>().onClick.AddListener(delegate { MainMenu.Instance.horseEquiped = skin.id; UpdateSkin(skin.name); });
             if (type == ItemType.Ball)
-                button.gameObject.GetComponent<Button>().onClick.AddListener(delegate { MainMenu.Instance.ballEquiped = skin.id; UpdateSkin(); });
+                button.gameObject.GetComponent<Button>().onClick.AddListener(delegate { MainMenu.Instance.ballEquiped = skin.id; UpdateSkin(skin.name); });
             if (type == ItemType.Rope)
-                button.gameObject.GetComponent<Button>().onClick.AddListener(delegate { MainMenu.Instance.ropeEquiped = skin.id; UpdateSkin(); });
+                button.gameObject.GetComponent<Button>().onClick.AddListener(delegate { MainMenu.Instance.ropeEquiped = skin.id; UpdateSkin(skin.name); });
             if (type == ItemType.Swing)
-                button.gameObject.GetComponent<Button>().onClick.AddListener(delegate { MainMenu.Instance.swingEquiped = skin.id; UpdateSkin(); });
+                button.gameObject.GetComponent<Button>().onClick.AddListener(delegate { MainMenu.Instance.swingEquiped = skin.id; UpdateSkin(skin.name); });
             if (type == ItemType.Platform)
-                button.gameObject.GetComponent<Button>().onClick.AddListener(delegate { MainMenu.Instance.platformEquiped = skin.id; UpdateSkin(); });
+                button.gameObject.GetComponent<Button>().onClick.AddListener(delegate { MainMenu.Instance.platformEquiped = skin.id; UpdateSkin(skin.name); });
         }
     }
 
-    public void UpdateSkin()
+    public void UpdateSkin(string name)
     {
         preview.SetActive(false); preview.SetActive(true);
         myButton.SetActive(false); myButton.SetActive(true);
+        myName.text = name;
     }
 }
